2e744d5d5172837d87f325e1a7d2cabaf7b9b61f
Amarnath <chinu230012@gmail.com>
2020-08-05 10:36:39 +0000

    add start script

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(6000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
